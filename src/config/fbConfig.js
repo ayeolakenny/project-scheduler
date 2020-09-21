4422877336111fe3e91f460075fb367b5472df26
Taiwo <ayeolakenny@gmail.com>
2020-09-21 02:38:32 +0100

    removed firebase config credentials

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -5,7 +5,7 @@ import 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
- // Add your firebase credentials here
+ // Add your firebase credentials here 
 };
 
 // Initialize Firebase
